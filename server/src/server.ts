36048fc7076a926bd4af7819127002465eb9d006
CesarAzambuja <caaso.eng@gmail.com>
2022-11-06 21:13:39 -0300

    Aula 4 - Conclusão do backEnd e rotas do aplicativo

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,61 +1,46 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { z } from 'zod'
-import { PrismaClient } from '@prisma/client'
-import ShortUniqueId from 'short-unique-id'
+import jwt from '@fastify/jwt'
 
-const prisma = new PrismaClient({
-    log: ['query'],
-})
+
+
+import { poolRoutes } from './routes/pool'
+import { userlRoutes } from './routes/user'
+import { guessRoutes } from './routes/guess'
+import { gameRoutes } from './routes/game'
+import { authRoutes } from './routes/auth'
+
 
 async function bootstrap() {
     const fastify = Fastify({
         logger: true,
     })
 
-    await fastify.register(cors, {
-        origin: true,
-    })
-
-    fastify.get('/pools/count', async () => {
-
-        const count = await prisma.pool.count()
-        return { count }
-    })
-
-    fastify.get('/users/count', async () => {
-
-        const count = await prisma.user.count()
-        return { count }
-    })
-
-    fastify.get('/guesses/count', async () => {
-
-        const count = await prisma.guess.count()
-        return { count }
-    })
-
-    fastify.post('/pools', async (request, reply) => {
-
-        const createPoolBody = z.object({
-            title: z.string()
+    try {
+        await fastify.register(cors, {
+            origin: true,
         })
-         const { title } = createPoolBody.parse(request.body)
 
-         const generate = new ShortUniqueId({ length: 6 })
-         const code =  String(generate()).toUpperCase() 
+        //Quando em produção a secret precisa ser uma variavel de ambiente.
 
-         await prisma.pool.create({
-            data: {
-                title,
-                code
-            }
-         })
+        await fastify.register(jwt, {
+            secret: 'nlwcopa',
+        })
 
-         return reply.status(201).send({ code })
-     })
+        await fastify.register(poolRoutes);
+        await fastify.register(authRoutes);
+        await fastify.register(gameRoutes);
+        await fastify.register(guessRoutes);
+        await fastify.register(userlRoutes);
 
-    await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ })
+
+        await fastify.listen({ port: 3333, host: '0.0.0.0' })
+    
+    }    catch (err){
+        fastify.log.error(err)
+
+        process.exit(1)
+    }
 }
 
 bootstrap()
